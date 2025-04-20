55cf602d7c415d3ddfa95b7d6e995500d8513c3e
Stephen Biko <biko.nyamai@gmail.com>
2025-04-20 23:07:39 +0300

    feat: update amplify configuration
    - Add a new amplify outputs configuration file
    - Modify amplify download script to use updated profile

--- portal/download_amplify_outputs.sh
+++ portal/download_amplify_outputs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #download amplify config
-npx ampx generate outputs --profile bikxs.net --app-id d2896e60a8d7f8 --branch main
+npx ampx generate outputs --profile shinrai.devpost --app-id d2896e60a8d7f8 --branch main
