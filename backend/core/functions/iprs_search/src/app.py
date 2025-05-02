54df5c33e26cef258db0224ff71d26688f1ceb74
effiecancode <faitheffie25@gmail.com>
2025-05-02 16:30:24 +0300

    feature: call all IPRS endpoint

--- backend/core/functions/iprs_search/src/app.py
+++ backend/core/functions/iprs_search/src/app.py
@@ -15,11 +15,38 @@ validator = JubileeESBAPI()
 def handler(event, context):
     try:
         logger.info(f"Received event: {event}")
-        iprs_search_result = validator.iprs.search_generic(event)
+        assert "method" in event, "Method key is not part of payload"
+
+        match event["method"]:
+            case "search_generic":
+                result = validator.iprs.search_generic(event)
+
+            case "ping":
+                result = validator.iprs.ping()
+
+            case "search_alien_id":
+                result = validator.iprs.search_alien_id(event)
+
+            case "search_passport_number":
+                result = validator.iprs.search_passport_number(event)
+
+            case "search_birth_certificate_number":
+                result = validator.iprs.search_birth_certificate_number(event)
+
+            case "search_death_certificate_number":
+                result = validator.iprs.search_id_number(event)
+
+            case "bulk_iprs_search":
+                result = validator.iprs.bulk_iprs_search(event["request_list"])
+
+
+            case _:
+                raise JubileeESBError(f"Method {event['method']} is not supported")
+
         return {
             "statusCode": 200,
             "body": json.dumps({
-                "result": iprs_search_result
+                "result": result
             })
         }
 
