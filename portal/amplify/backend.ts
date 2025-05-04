54d8fa6d7b1570052ac27b381e7ee2273e333ce2
Stephen Biko <biko.nyamai@gmail.com>
2025-05-04 16:44:03 +0300

    feat(backend): Add EKYC storage resources
    - Import and add `eKycCertificateStorage` and `eKycDocumentStorage` to the backend configuration.
    - Create new `storage/resource.ts` file with EKYC storage definitions.
    
    These changes enable the handling of eKYC documents and certificates within the application.

--- portal/amplify/backend.ts
+++ portal/amplify/backend.ts
@@ -1,8 +1,11 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import {defineBackend} from '@aws-amplify/backend';
+import {auth} from './auth/resource';
+import {data} from './data/resource';
+import {eKycCertificateStorage, eKycDocumentStorage} from './storage/resource';
 
 defineBackend({
-  auth,
-  data,
+    auth,
+    data,
+    eKycDocumentStorage,
+    eKycCertificateStorage
 });
